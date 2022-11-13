77eb3919c788ef6fd090a97188bc303b68757d57
Alexandra Grenader <Alexandrag@ayehu.com>
2022-11-13 14:52:41 +0200

    Add project files.

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SalesDal;
+
+namespace SalesCoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ICitiesService _citiesService;
+
+        public CitiesController(ICitiesService citiesService)
+        {
+            _citiesService = citiesService;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetCities()
+        {
+            var cities = _citiesService.GetCities();
+            var strCities = cities.Select(l => l.CityName);
+            return strCities;
+        }
+    }
+}
